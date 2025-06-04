4e60bdf7fb7d4dac35ae3df56c656f5142344fcb
wwwalohacampus <h850415@naver.com>
2025-06-04 15:51:11 +0900

    TodoList 프로젝트 2

--- TodoList/todo/urls.py
+++ TodoList/todo/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('todo', views.todo, name='todo'),
     path('create', views.create, name='create'),
     path('delete', views.delete, name='delete'),
+    path('ing', views.ing, name='ing'),
+    path('done', views.done, name='done'),
+    path('wait', views.wait, name='wait'),
 ]
--- TodoList/todo/views.py
+++ TodoList/todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.db.models import Q
 
 from .models import *   # models 의 모든 모델 import
 
@@ -12,8 +13,13 @@ def index(request):
 def todo(request):
     print('할 일 목록 화면...')
     # 할 일 목록 조회
-    todos = Todo.objects.all()  # Todo 모델의 모든 데이터 조회
-    content = {'todos': todos}
+    # Todo 모델의 대기 목록 조회
+    wait_list = Todo.objects.filter(status='WAIT')
+    # Todo 모델의 진행 목록 조회
+    ing_list = Todo.objects.filter(Q(status='ING') | Q(status='DONE')).order_by('-status')
+    
+    
+    content = {'wait_list': wait_list, 'ing_list': ing_list}
     # render(request, 템플릿 경로, 데이터{})
     # - 데이터{} : 템플릿에 데이터를 전달
     return render(request, 'todo/todo.html', content)
@@ -40,3 +46,48 @@ def delete(request):
     except Todo.DoesNotExist:
         print('삭제할 할 일이 없습니다.')
     return HttpResponseRedirect(reverse('todo'))
+
+def ing(request):
+    print('진행 상태로 변경...')
+    no = request.POST['no']
+    print('no : {}'.format(no))
+    try:
+        todo = Todo.objects.get(no=no)
+        # 할 일 상태 수정
+        todo.status = 'ING' 
+        todo.save()
+    except Todo.DoesNotExist:
+        print('수정할 할 일이 없습니다.')
+    return HttpResponseRedirect(reverse('todo'))
+
+
+def done(request):
+    print('완료 상태로 변경...')
+    no = request.POST['no']
+    print('no : {}'.format(no))
+    try:
+        todo = Todo.objects.get(no=no)
+        # 할 일 상태 수정
+        if todo.status == 'DONE':
+            todo.status = 'ING' 
+        else:
+            todo.status = 'DONE' 
+        todo.save()
+    except Todo.DoesNotExist:
+        print('수정할 할 일이 없습니다.')
+    return HttpResponseRedirect(reverse('todo'))
+
+
+def wait(request):
+    print('대기 상태로 변경...')
+    no = request.POST['no']
+    print('no : {}'.format(no))
+    try:
+        todo = Todo.objects.get(no=no)
+        # 할 일 상태 수정
+        todo.status = 'WAIT' 
+        todo.save()
+    except Todo.DoesNotExist:
+        print('수정할 할 일이 없습니다.')
+    return HttpResponseRedirect(reverse('todo'))
+    
